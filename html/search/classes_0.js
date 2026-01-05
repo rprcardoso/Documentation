a80f4b41d33a046d1e6c65657bd8ac76482aa90a
rprcardoso <168768892+rprcardoso@users.noreply.github.com>
2026-01-05 17:54:28 +0000

    with part of MRESS

--- html/search/classes_0.js
+++ html/search/classes_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['nonlinearshellmcyse_0',['NonlinearShellMCYSE',['../classNonlinearShellMCYSE.html',1,'']]],
-  ['nonlinearshellmitc4_1',['NonlinearShellMITC4',['../classNonlinearShellMITC4.html',1,'']]]
+  ['nonlinearmress_0',['NonlinearMRESS',['../classNonlinearMRESS.html',1,'']]],
+  ['nonlinearshellmcyse_1',['NonlinearShellMCYSE',['../classNonlinearShellMCYSE.html',1,'']]],
+  ['nonlinearshellmitc4_2',['NonlinearShellMITC4',['../classNonlinearShellMITC4.html',1,'']]]
 ];
--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['set_5flocalaxes_0',['set_LocalAxes',['../classNonlinearShellMCYSE.html#a0f14ac0e4254808f85d2c82b8b9410f9',1,'NonlinearShellMCYSE']]],
+  ['set_5flocalaxes_0',['set_localaxes',['../classNonlinearShellMCYSE.html#a0f14ac0e4254808f85d2c82b8b9410f9',1,'NonlinearShellMCYSE::set_LocalAxes()'],['../classNonlinearMRESS.html#a7a04b0151d374bb1dfc5f55fd812bb23',1,'NonlinearMRESS::set_LocalAxes()']]],
   ['set_5flocalaxes_5fall_1',['set_LocalAxes_All',['../classNonlinearShellMITC4.html#acc4a67f21b10bc267c05707aef78c0d4',1,'NonlinearShellMITC4']]]
 ];
