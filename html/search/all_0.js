db9ee3fb25a1cc9bea11fedc716df068bedbb642
rprcardoso <168768892+rprcardoso@users.noreply.github.com>
2026-01-21 16:03:09 +0000

    MRESS updated 21Jan2026

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -4,6 +4,10 @@ var searchData=
   ['calculate_5finternal_5fforces_5fexplicit_1',['Calculate_Internal_Forces_Explicit',['../classNonlinearShellMCYSE.html#a1107fac85d92b87059dab0907ac002bf',1,'NonlinearShellMCYSE']]],
   ['calculate_5flocal_5faxes_5fall_5fgp_2',['Calculate_Local_Axes_All_GP',['../classNonlinearShellMITC4.html#ae62ea52a3008714c27f34f7e2aa2734a',1,'NonlinearShellMITC4']]],
   ['calculate_5fstiffness_5finternal_3',['Calculate_Stiffness_Internal',['../classNonlinearShellMITC4.html#af6d8c272935871606caaf2a235a29031',1,'NonlinearShellMITC4']]],
-  ['calculate_5fstiffnessmcyse_4',['Calculate_StiffnessMCYSE',['../classNonlinearShellMCYSE.html#a7441086666291b95a0fd6fbc8996eefc',1,'NonlinearShellMCYSE']]],
-  ['calculate_5fstiffnessmitc4_5',['Calculate_StiffnessMITC4',['../classNonlinearShellMITC4.html#aeff6c53a3abcd7159dded289c9b322a4',1,'NonlinearShellMITC4']]]
+  ['calculate_5fstiffness_5finternal_5fforce_4',['Calculate_Stiffness_Internal_Force',['../classNonlinearMRESS.html#abca67b17215ffced172821271af606c9',1,'NonlinearMRESS']]],
+  ['calculate_5fstiffnessinitialstress_5',['calculate_StiffnessInitialStress',['../classNonlinearMRESS.html#a80fe655eea42c09d3e605061ac042f0b',1,'NonlinearMRESS']]],
+  ['calculate_5fstiffnessmcyse_6',['Calculate_StiffnessMCYSE',['../classNonlinearShellMCYSE.html#a7441086666291b95a0fd6fbc8996eefc',1,'NonlinearShellMCYSE']]],
+  ['calculate_5fstiffnessmitc4_7',['Calculate_StiffnessMITC4',['../classNonlinearShellMITC4.html#aeff6c53a3abcd7159dded289c9b322a4',1,'NonlinearShellMITC4']]],
+  ['calculate_5fstifness_5fcomposites_5fmress_8',['Calculate_Stifness_Composites_MRESS',['../classNonlinearMRESS.html#a2ac381fcc57f4147fdc1b602bfb51f33',1,'NonlinearMRESS']]],
+  ['calculate_5fstifness_5fmress_9',['Calculate_Stifness_MRESS',['../classNonlinearMRESS.html#a3f62c3f6beb7f9d4d7f32f64325431f6',1,'NonlinearMRESS']]]
 ];
